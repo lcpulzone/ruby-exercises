3e3aa95d68a49973083c44ee91437d499b6a943d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-10-27 11:46:39 -0700

    Skip tests that are not being worked on

--- mythical-creatures/spec/vampire_spec.rb
+++ mythical-creatures/spec/vampire_spec.rb
@@ -7,28 +7,28 @@ RSpec.describe Vampire do
     expect(vampire.name).to eq('Dracula')
   end
 
-  it 'it can be named something else' do
+  xit 'it can be named something else' do
     vampire = Vampire.new('Vladimir')
     expect(vampire.name).to eq('Vladimir')
   end
 
-  it 'keeps a pet bat by default' do
+  xit 'keeps a pet bat by default' do
     vampire = Vampire.new('Ruthven')
     expect(vampire.pet).to eq('bat')
   end
 
-  it 'can keep other pets' do
+  xit 'can keep other pets' do
     vampire = Vampire.new('Varney', 'fox')
     expect(vampire.pet).to eq('fox')
   end
 
-  it 'is thirsty by default' do
+  xit 'is thirsty by default' do
     vampire = Vampire.new('The Count')
 
     expect(vampire.thirsty).to be true
   end
 
-  it 'is not thirsty after drinking' do
+  xit 'is not thirsty after drinking' do
     vampire = Vampire.new('Elizabeth Bathory')
 
     vampire.drink
