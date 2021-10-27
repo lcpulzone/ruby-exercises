4a402a4873fb63c9e4295bb67eaa999fd1977061
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-10-27 10:27:12 -0700

    Add color instance variable and attr_reader, create silver? method

--- mythical-creatures/lib/unicorn.rb
+++ mythical-creatures/lib/unicorn.rb
@@ -1,7 +1,12 @@
 class Unicorn
-  attr_reader :name
+  attr_reader :name, :color
 
   def initialize(name)
     @name = name
+    @color = 'silver'
+  end
+
+  def silver?
+    @color == 'silver'
   end
 end
