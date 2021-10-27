763d8be37ea7aeb9b301e5fb101af759bcfe5dba
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-10-27 12:16:59 -0700

    Add run method

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -1,9 +1,10 @@
 class Centaur
-    attr_reader :name, :breed, :shoot
+    attr_reader :name, :breed, :shoot, :run
 
     def initialize(name, breed)
         @name = name
         @breed = breed
         @shoot = "Twang!!!"
+        @run = "Clop clop clop clop!"
     end
 end
