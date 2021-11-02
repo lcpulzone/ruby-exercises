d432ba7b371656c4a1a6e40c04bc33178332b351
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-02 16:44:05 -0700

    Add conditional to run method

--- mythical-creatures/lib/centaur.rb
+++ mythical-creatures/lib/centaur.rb
@@ -1,54 +1,58 @@
 class Centaur
-    attr_reader :name, :breed
+  attr_reader :name, :breed
 
-    def initialize(name, breed)
-        @name = name
-        @breed = breed
-        @activity_count = 0
-        @laying_count = 0
-    end
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+    @activity_count = 0
+    @laying_count = 0
+  end
 
-    def cranky?
-        if @activity_count >=3 
-            true
-        else
-            false
-        end
+  def cranky?
+    if @activity_count >=3 
+      true
+    else
+      false
     end
+  end
 
-    def standing?
-        if @laying_count == 0
-            true
-        else
-            false
-        end
+  def standing?
+    if @laying_count == 0
+      true
+    else
+      false
     end
+  end
 
-    def shoot
-        @activity_count += 1
-        if cranky? == true
-            "NO!"
-        else
-            "Twang!!!"
-        end
+  def shoot
+    @activity_count += 1
+    if cranky? == true
+      "NO!"
+    else
+      "Twang!!!"
     end
+  end
 
-    def run
-        @activity_count += 1
-        "Clop clop clop clop!"
-    end
+  def run
+    @activity_count += 1
+		if laying? 
+			"NO!"
+		else
+			"Clop clop clop clop!"
+		end
+  end
 
-    def sleep
-        if standing? == true
-            "NO!"
-        end
+  def sleep
+    if standing? == true
+      "NO!"
     end
+  end
 
-    def lay_down
-        @laying_count += 1
-    end
+  def lay_down
+    @laying_count += 1
+  end
 
-    def laying?
-        return true if @laying_count >= 1
-    end
+  def laying?
+    return true if @laying_count >= 1
+  end
 end
