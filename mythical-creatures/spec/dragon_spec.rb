f88d95567c240ed316dd37455a72dc0e83b562eb
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-08 15:55:31 -0800

    Skip tests for ease of testing

--- mythical-creatures/spec/dragon_spec.rb
+++ mythical-creatures/spec/dragon_spec.rb
@@ -7,37 +7,37 @@ RSpec.describe Dragon do
     expect(dragon.name).to eq('Ramoth')
   end
 
-  it 'has a rider' do
+  xit 'has a rider' do
     dragon = Dragon.new('Ramoth', :gold, 'Lessa')
     expect(dragon.rider).to eq('Lessa')
   end
 
-  it 'has a color' do
+  xit 'has a color' do
     dragon = Dragon.new('Ramoth', :gold, 'Lessa')
     expect(dragon.color).to eq(:gold)
   end
 
-  it 'is a different dragon' do
+  xit 'is a different dragon' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.name).to eq('Mnementh')
   end
 
-  it 'has a different rider' do
+  xit 'has a different rider' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.rider).to eq('Flar')
   end
 
-  it 'has a different color' do
+  xit 'has a different color' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.color).to eq(:bronze)
   end
 
-  it 'was born hungry' do
+  xit 'was born hungry' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.hungry?).to be true
   end
 
-  it 'eats a lot' do
+  xit 'eats a lot' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.hungry?).to be true
     dragon.eat
