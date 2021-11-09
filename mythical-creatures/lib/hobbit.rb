4ce9be90f751b79f870eb357e192132fbf9b3b6d
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-08 16:18:43 -0800

    Create age and celebrate_birthday method

--- mythical-creatures/lib/hobbit.rb
+++ mythical-creatures/lib/hobbit.rb
@@ -1,8 +1,13 @@
 class Hobbit
-	attr_reader :name, :disposition
+	attr_reader :name, :disposition, :age
 
 	def initialize(name, disposition = "homebody")
 		@name = name 
 		@disposition = disposition
+		@age = 0
+	end
+
+	def celebrate_birthday
+		@age += 1
 	end
 end
