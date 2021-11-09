4c160f954c7655ff4e6b94a0b7a3dd3e112c64fe
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-08 16:28:50 -0800

    Create has_ring test

--- mythical-creatures/spec/hobbit_spec.rb
+++ mythical-creatures/spec/hobbit_spec.rb
@@ -62,7 +62,7 @@ RSpec.describe Hobbit do
     hobbit = Hobbit.new('Otho')
 
 		expect(hobbit.old?).to be false
-		
+
     101.times do
 			hobbit.celebrate_birthday
 		end
@@ -70,11 +70,12 @@ RSpec.describe Hobbit do
 		expect(hobbit.old?).to be true
   end
 
-  xit 'it has the ring if its name is Frodo' do
-    # create a hobbit named Frodo
-    # create a second hobbit named Sam
-    # check that .has_ring? for Frodo returns true
-    # check that .has_ring? for Sam returns false
+  it 'it has the ring if its name is Frodo' do
+    frodo = Hobbit.new('Frodo')
+    sam = Hobbit.new('Sam')
+
+		expect(frodo.has_ring?).to be true
+		expect(sam.has_ring?).to be false
   end
 
   xit 'they are short' do
