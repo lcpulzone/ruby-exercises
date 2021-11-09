19f78963b6183b19263fa6a1ef2c3d6f23919529
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-08 16:04:01 -0800

    Create hungry and eat methods

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -1,9 +1,23 @@
 class Dragon
-	attr_reader :name, :color, :rider
+	attr_reader :name, :color, :rider, :hunger_level
 	
 	def initialize(name, color, rider)
 		@name = name
 		@color = color
 		@rider = rider
+		@hunger_level = 0
 	end
+
+	def hungry?
+		if @hunger_level <= 2
+			true 
+		else
+			false
+		end
+	end
+
+	def eat
+		@hunger_level += 1
+	end
+	
 end
