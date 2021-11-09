eb26dce5595daf301093a86877608520df995066
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-09 15:22:25 -0800

    Skip tests for ease of testing printout

--- mythical-creatures/spec/pirate_spec.rb
+++ mythical-creatures/spec/pirate_spec.rb
@@ -7,22 +7,22 @@ RSpec.describe Pirate do
     expect(pirate.name).to eq('Jane')
   end
 
-  it 'can have a different name' do
+  xit 'can have a different name' do
     pirate = Pirate.new('Blackbeard')
     expect(pirate.name).to eq('Blackbeard')
   end
 
-  it 'is a scallywag by default' do
+  xit 'is a scallywag by default' do
     pirate = Pirate.new('Jane')
     expect(pirate.job).to eq('Scallywag')
   end
 
-  it 'is not always a scallywag' do
+  xit 'is not always a scallywag' do
     pirate = Pirate.new('Jack', 'cook')
     expect(pirate.job).to eq('cook')
   end
 
-  it 'is not cursed by default' do
+  xit 'is not cursed by default' do
     pirate = Pirate.new('Jack')
 
     expect(pirate.cursed?).to be false
@@ -37,15 +37,14 @@ RSpec.describe Pirate do
     expect(pirate.cursed?).to be true
   end
 
-  it 'has a booty' do
+  xit 'has a booty' do
     # create a pirate
     # check that the pirate starts with 0 booty
   end
 
-  it 'gets 100 booty for robbing a ship' do
+  xit 'gets 100 booty for robbing a ship' do
     # create a pirate
     # rob some ships
     # check that the pirate got 100 booty for each ship it robbed
   end
-
 end
