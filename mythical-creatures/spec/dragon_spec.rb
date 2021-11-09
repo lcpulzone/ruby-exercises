1c497d5ab796f5d15f1aaac594420fef747f5d85
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-08 16:00:41 -0800

    Remove skip from test

--- mythical-creatures/spec/dragon_spec.rb
+++ mythical-creatures/spec/dragon_spec.rb
@@ -12,27 +12,27 @@ RSpec.describe Dragon do
     expect(dragon.rider).to eq('Lessa')
   end
 
-  xit 'has a color' do
+  it 'has a color' do
     dragon = Dragon.new('Ramoth', :gold, 'Lessa')
     expect(dragon.color).to eq(:gold)
   end
 
-  xit 'is a different dragon' do
+  it 'is a different dragon' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.name).to eq('Mnementh')
   end
 
-  xit 'has a different rider' do
+  it 'has a different rider' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.rider).to eq('Flar')
   end
 
-  xit 'has a different color' do
+  it 'has a different color' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.color).to eq(:bronze)
   end
 
-  xit 'was born hungry' do
+  it 'was born hungry' do
     dragon = Dragon.new('Mnementh', :bronze, 'Flar')
     expect(dragon.hungry?).to be true
   end
