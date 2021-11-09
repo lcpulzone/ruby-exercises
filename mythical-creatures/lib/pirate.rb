9e6c08c6ca4d2443f9484812bd9b8d722cffa1ba
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-09 15:30:26 -0800

    Create cursed and heinous acts methods

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -4,6 +4,15 @@ class Pirate
 	def initialize(name, job = "Scallywag")
 		@name = name
 		@job = job
+		@acts_commited = 0
+	end
+	
+	def cursed?
+		@acts_commited >= 3 
+	end
+	
+	def commit_heinous_act
+		@acts_commited += 1
 	end
 	
 end
