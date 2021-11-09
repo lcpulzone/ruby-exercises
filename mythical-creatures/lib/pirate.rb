242b3553cb399ef806cc17c57a86a09a1d5db696
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-09 15:32:07 -0800

    Create booty instance variable set to 0

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -1,10 +1,11 @@
 class Pirate
-	attr_reader :name, :job
+	attr_reader :name, :job, :booty
 
 	def initialize(name, job = "Scallywag")
 		@name = name
 		@job = job
 		@acts_commited = 0
+		@booty = 0
 	end
 	
 	def cursed?
