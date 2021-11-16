1a4b7faddf8164fd386506561f4310a09caede40
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-16 15:39:24 -0800

    Create Person class and stare method

--- mythical-creatures/lib/medusa.rb
+++ mythical-creatures/lib/medusa.rb
@@ -5,4 +5,17 @@ class Medusa
 		@name = name 
 		@statues = []
 	end
+
+	def stare(victim)
+		@statues << victim
+	end
+		
+end
+
+class Person
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+	end
 end
