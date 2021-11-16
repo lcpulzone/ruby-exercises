436a8ddbd28df00fb0e272b42f3892c16d704422
Leigh Cepriano Pulzone <lcpulzone@gmail.com>
2021-11-16 15:30:04 -0800

    Skip tests for ease

--- mythical-creatures/spec/medusa_spec.rb
+++ mythical-creatures/spec/medusa_spec.rb
@@ -7,12 +7,12 @@ RSpec.describe Medusa do
     expect(medusa.name).to eq('Cassiopeia')
   end
 
-  it 'has no statues when created' do
+  xit 'has no statues when created' do
     medusa = Medusa.new('Cassiopeia')
     expect(medusa.statues).to be_empty
   end
 
-  it 'gains a statue when staring at a person' do
+  xit 'gains a statue when staring at a person' do
     medusa = Medusa.new('Cassiopeia')
     victim = Person.new('Perseus')
 
@@ -22,7 +22,7 @@ RSpec.describe Medusa do
     expect(medusa.statues.first).to be_an_instance_of(Person)
   end
 
-  it 'turns a person to stone when staring at them' do
+  xit 'turns a person to stone when staring at them' do
     medusa = Medusa.new('Cassiopeia')
     victim = Person.new('Perseus')
 
@@ -31,11 +31,11 @@ RSpec.describe Medusa do
     assert(victim.stoned?).to be true
   end
 
-  it 'can only have three victims' do
+  xit 'can only have three victims' do
     # your code here
   end
 
-  it 'if a fourth victim is stoned the first is unstoned' do
+  xit 'if a fourth victim is stoned the first is unstoned' do
     # your code here
   end
 end
